c60ac699ba845925e7abb9b5c3835bb09115f1df
Daniel GOMEZ FERNANDEZ <g_f_daniel@hotmail.com>
2021-11-10 15:47:59 +0100

    bootstrap colors component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BootstrapGridComponent } from './bootstrap-grid/bootstrap-grid.component';
+import { BootstrapColorsComponent } from './bootstrap-colors/bootstrap-colors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BootstrapGridComponent
+    BootstrapGridComponent,
+    BootstrapColorsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/bootstrap-colors/bootstrap-colors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bootstrap-colors',
+  templateUrl: './bootstrap-colors.component.html',
+  styleUrls: ['./bootstrap-colors.component.scss']
+})
+export class BootstrapColorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

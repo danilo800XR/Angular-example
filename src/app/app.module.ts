9ba36320b8b6b23a612b3143ea54b969782c35e9
Daniel GOMEZ FERNANDEZ <g_f_daniel@hotmail.com>
2021-11-10 15:33:57 +0100

    bootstrap-grid component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BootstrapGridComponent } from './bootstrap-grid/bootstrap-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BootstrapGridComponent
   ],
   imports: [
     BrowserModule,

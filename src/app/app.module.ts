4350284e2b30d7805d764553cd3cd6cdc3549a4a
Daniel GOMEZ FERNANDEZ <g_f_daniel@hotmail.com>
2021-11-11 12:26:34 +0100

    bootstrap display component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BootstrapGridComponent } from './components/bootstrap-grid/bootstrap-grid.component';
 import { BootstrapColorsComponent } from './components/bootstrap-colors/bootstrap-colors.component';
 import { BootstrapSpacingComponent } from './components/bootstrap-spacing/bootstrap-spacing.component';
+import { BootstrapDisplayComponent } from './components/bootstrap-display/bootstrap-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BootstrapGridComponent,
     BootstrapColorsComponent,
-    BootstrapSpacingComponent
+    BootstrapSpacingComponent,
+    BootstrapDisplayComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/bootstrap-display/bootstrap-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bootstrap-display',
+  templateUrl: './bootstrap-display.component.html',
+  styleUrls: ['./bootstrap-display.component.scss']
+})
+export class BootstrapDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
